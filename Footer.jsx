1cde1db87281246f4465a02cbbe1f2708076e450
Swathi-Nakkiran <86572741+Swathi-Nakkiran@users.noreply.github.com>
2021-06-28 01:44:06 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>
+        <i>Copyright by ShapeAI @ {new Date().getFullYear()}</i>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
